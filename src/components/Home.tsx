1f674f259baf97cda4c58bea6f85269950d911a9
Brandon <brandonbgrayson@gmail.com>
2022-09-27 22:36:17 -0400

    navbar stub

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,9 @@
+import Navbar from "./Navbar";
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <Navbar />
+      <h1>Home</h1>
+    </>
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+export default function Navbar() {
+  return (
+    <Grid container sx={{ height: "100px", backgroundColor: "red" }}>
+      <Grid item xs={12}></Grid>
+    </Grid>
+  );
+}
